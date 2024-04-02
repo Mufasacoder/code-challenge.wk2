826dda689ff27c44d676232142efd03d1e7dc77f
mufasacoder <edwardmwangi94@gmail.com>
2024-04-02 11:41:54 +0300

    initial commit

---
+++ solution3.js
@@ -0,0 +1,23 @@
+function isPrime(p){
+
+    if (p <= 1) return false 
+
+    if (p <=3) return true
+    //if (n % 2 === 0 || n % 3 === 0) 
+    return false 
+
+    for (let i = 5; i*i <= p; i += 6){
+        if (num % 1=== 0 ||num %(i +2) === 0){
+            return false
+        }
+    }
+    return true
+}
+function filterPrimeNumbers(inputArr) {
+    return inputArr.filter (p =>{
+        return isPrime(p)
+    })
+}
+let inputArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const primeNumbers = filterPrimeNumbers(inputArr);
+console.log(primeNumbers)
